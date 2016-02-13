c0ab1223134a846e822265b29f2997c4a30561ce
xkxx <xukexiang@gmail.com>
2016-02-13 15:25:43 -0800

    rewrite to use prototype instead of closure for perf; add parse.stream for parsing streams (experimental)

--- index.js
+++ index.js
@@ -2,53 +2,86 @@ var htmlparser = require('htmlparser2');
 var assert = require('assert');
 var debuglog = require('util').debuglog('2vdom');
 
-var parse = function (jsxHandler, html) {
-  var tree_stack = [];
-  var result_tree = null;
+VDomHandler = function(jsxHandler) {
+  this.jsxHandler = jsxHandler;
+  this.treeStack = [];
+  this.resultTree = null;
+}
 
-  var vDomHandler = {
-    onopentag: function(name, attribs) {
-      debuglog('open tag:', name, attribs);
-      tree_stack.push([name, attribs]);
-    },
-    ontext: function(text) {
-      debuglog('text:', text);
-      assert.notEqual(tree_stack.length, 0,
-        "HTML root level cannot contain text node");
+VDomHandler.prototype = {
+  onopentag: function(name, attribs) {
+    debuglog('open tag:', name, attribs);
+    this.treeStack.push([name, attribs]);
+  },
+  ontext: function(text) {
+    debuglog('text:', text);
+    var treeStack = this.treeStack;
+    assert.notEqual(treeStack.length, 0,
+      "HTML root level cannot contain text node");
 
-      var parent = tree_stack[tree_stack.length - 1];
-      parent.push(text);
-    },
-    onclosetag: function(name) {
-      assert.notEqual(tree_stack.length, 0,
-        "Can't close tags without opening one");
+    var parent = treeStack[treeStack.length - 1];
+    parent.push(text);
+  },
+  onclosetag: function(name) {
+    var treeStack = this.treeStack;
+    assert.notEqual(treeStack.length, 0,
+      "Can't close tags without opening one");
 
-      var node_args = tree_stack.pop();
+      var nodeArgs = treeStack.pop();
 
-      assert.equal(name, node_args[0],
-        "Opening and closing tagname must match");
+    assert.equal(name, nodeArgs[0],
+      "Opening and closing tagname must match");
 
-      var vnode = jsxHandler.apply(null, node_args);
+    var vnode = this.jsxHandler.apply(null, nodeArgs);
 
-      if (tree_stack.length === 0) {
-        result_tree = vnode;
-      }
-      else {
-        var parent = tree_stack[tree_stack.length - 1];
-        parent.push(vnode);
-      }
-    },
-    // TODO: how to handle doctype?
-    onprocessinginstruction: function(name, data) {
-      debuglog('directive: ', name, data);
-    },
-  }
+    if (treeStack.length === 0) {
+      this.resultTree = vnode;
+    }
+    else {
+      var parent = treeStack[treeStack.length - 1];
+      parent.push(vnode);
+    }
+  },
+  // TODO: how to handle doctype?
+  onprocessinginstruction: function(name, data) {
+    debuglog('directive: ', name, data);
+  },
+};
 
+var parse = function parse(jsxHandler, html) {
+  var vDomHandler = new VDomHandler(jsxHandler);
   var parser = new htmlparser.Parser(vDomHandler);
   parser.write(html);
   parser.end();
 
-  return result_tree;
-}
+  return vDomHandler.resultTree;
+};
+
+parse.stream = function parseStream(jsxHandler, stream) {
+  return new Promise(function(resolve, reject) {
+    var vDomHandler = new VDomHandler(jsxHandler);
+    var parser = new htmlparser.Parser(vDomHandler);
+    stream.on('data', function(data) {
+      try {
+        parser.write(data);
+      }
+      catch (err) {
+        reject(err);
+      }
+    });
+    stream.on('error', function(err) {
+      reject(err);
+    });
+    stream.on('end', function() {
+      try {
+        parser.end();
+        resolve(vDomHandler.resultTree);
+      }
+      catch (err) {
+        reject(err);
+      }
+    });
+  });
+};
 
 module.exports = parse;
